bf5ebf6b4655119d5b2215c43ce0c524f501dc47
SL_JaNa <93564410+SLJaNa@users.noreply.github.com>
2021-11-03 16:31:38 +0530

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -19,15 +19,15 @@ class Scripted(object):
 
 
     ABOUT_TEXT = """
-<b>🤖 𝐌𝐲 𝐍𝐚𝐦𝐞 : <a href='https://t.me/Renamer_teleroid_bot'>Rename X2 Bot</a></b>\n
-<b>📢 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 : <a href='https://t.me/TeleRoidGroup'>TÉLÉRØÎD</a></b>\n
-<b>👥 𝐕𝐞𝐫𝐬𝐢𝐨𝐧 𝐕𝟐 : <a href='https://t.me/TeleRoid_Renamer_bot'>0.9.2 beta</a></b>\n
-<b>📥 𝐒𝐨𝐮𝐫𝐜𝐞 : <a href='https://github.com/P-Phreak/Renamer-bot'>Click Here</a></b>\n
+<b>🤖 𝐌𝐲 𝐍𝐚𝐦𝐞 : <a href='http://t.me/SL_Jana_Rename_Bot'>SL_Jana_Rename_Bot</a></b>\n
+<b>📢 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 : <a href='https://t.me/SL_Jana_Team'>SL_Jana_Team</a></b>\n
+<b>👥 𝐕𝐞𝐫𝐬𝐢𝐨𝐧 𝐕𝟐 : <a href='http://t.me/SL_Jana_Rename_Bot'>0.9.2 beta</a></b>\n
+<b>📥 𝐒𝐨𝐮𝐫𝐜𝐞 : <a href='https://github.com/SLJaNa/Renamer-Bot'>Click Here</a></b>\n
 <b>🌐 𝐒𝐞𝐫𝐯𝐞𝐫 : <a href='https://heroku.com'>Heroku</a></b>\n
-<b>📕 𝐋𝐢𝐛𝐫𝐚𝐫𝐲 : <a href='https://github.com/pyrogram'>Pyrogram 1.2.8</a></b>\n
+<b>📕 𝐋𝐢𝐛𝐫𝐚𝐫𝐲 : <a href='https://t.me/SL_Jana_Team'>SL_Jana_Team</a></b>\n
 <b>㊙ 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞  : <a href='https://www.python.org'>Python 3.9.4</a></b>\n
-<b>👨‍💻 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫 : <a href='https://t.me/PredatorHackerzZ_bot'>꧁ ƤℜɆĐ₳₮Øℜ 🇮🇳 ꧂</a></b>\n
-<b>📌 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 : <a href='https://t.me/Moviesflixers_DL'>Tᴀᴍɪʟᴡᴇʙ Tɢ Nᴇᴛᴡᴏʀᴋ</a></b>\n"""
+<b>👨‍💻 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫 : <a href='https://t.me/SL_Jana_Team'>꧁ SL_Jana_Team ꧂</a></b>\n
+<b>📌 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 : <a href='https://t.me/SL_Jana_Team'>꧁ SL_Jana_Team ꧂</a></b>\n"""
 
 
     CUSTOM_CAPTION = "<i>{}</i>"
@@ -37,14 +37,14 @@ class Scripted(object):
     CURRENT_THUMBNAIL = "<i>𝐘𝐨𝐮𝐫 𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 🎭</i>"
     THUMBNAIL_SAVED = "<i>𝐘𝐨𝐮𝐫 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐒𝐚𝐯𝐞𝐝 ✅</i>"
     THUMBNAIL_DELETED = "<i>𝐘𝐨𝐮𝐫 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐃𝐞𝐥𝐞𝐭𝐞𝐝 ✅</i>"
-    NO_THUMBNAIL_FOUND = "<i>𝐍𝐨 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐅𝐨𝐮𝐧𝐝 (𝐊𝐨𝐧𝐬𝐢 𝐆𝐚𝐚𝐥𝐢 𝐂𝐡𝐚𝐡𝐢𝐲𝐞)😔</i>"
+    NO_THUMBNAIL_FOUND = "<i>𝐍𝐨 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐅𝐨𝐮𝐧𝐝 😔</i>"
     TRYING_TO_DOWNLOAD = "<i>𝐓𝐫𝐲𝐢𝐧𝐠 𝐓𝐨 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝....</i>"
-    UPLOAD_SUCCESS = "<u><i>𝕿𝖍𝖆𝖓𝖐𝖘 𝖋𝖔𝖗 𝖚𝖘𝖎𝖓𝖌 𝖒𝖊𝖍 𝖇𝖔𝖙 ❤ @TheTeleRoid</i></u>"
+    UPLOAD_SUCCESS = "<u><i>❣️𝓣𝓱𝓪𝓷𝓴𝓼 𝓕𝓸𝓻 𝓤𝓼𝓲𝓷𝓰 𝓜𝓮❣️@SL_Jana_Rename_Bot</i></u>"
     REPLY_TO_MEDIA = "<i>𝐑𝐞𝐩𝐥𝐲 𝐭𝐨 𝐭𝐡𝐚𝐭 𝐌𝐞𝐝𝐢𝐚 𝐰𝐢𝐭𝐡 /convert</i>"
     UPLOAD_START = "<i>📤 𝐔𝐩𝐥𝐨𝐚𝐝𝐢𝐧𝐠 𝐲𝐨𝐮𝐫 𝐟𝐢𝐥𝐞 𝐩𝐥𝐞𝐚𝐬𝐞 𝐰𝐚𝐢𝐭...</i>\n"
     DOWNLOAD_START = "<i>📥 𝐃𝐨𝐰𝐧𝐥𝐨𝐝𝐢𝐧𝐠 𝐲𝐨𝐮𝐫 𝐟𝐢𝐥𝐞 𝐩𝐥𝐞𝐚𝐬𝐞 𝐰𝐚𝐢𝐭...</i>\n"
-    JOIN_NOW_TEXT = "<code>𝕱𝖎𝖗𝖘𝖙 𝕵𝖔𝖎𝖓 𝕸𝖞 𝖀𝖕𝖉𝖆𝖙𝖊𝖘 𝕮𝖍𝖆𝖓𝖓𝖊𝖑 𝕿𝖔 𝖀𝖘𝖊 𝕸𝖊𝖍</code>"
+    JOIN_NOW_TEXT = "<code>💢𝔽𝕚𝕣𝕤𝕥 𝕁𝕠𝕚𝕟 𝕄𝕪 𝕌𝕡𝕕𝕒𝕥𝕖𝕤 ℂ𝕙𝕒𝕟𝕟𝕖𝕝 𝕋𝕠 𝕌𝕤𝕖 𝕄𝕖💢@SL_Jana_Team</code>"
     REPLY_TO_FILE = "<i>𝐑𝐞𝐩𝐥𝐲 𝐭𝐨 𝐭𝐡𝐚𝐭 𝐟𝐢𝐥𝐞 𝐰𝐢𝐭𝐡 /rename 𝐧𝐞𝐰 𝐧𝐚𝐦𝐞 .𝐞𝐱𝐭</i>"
-    CONTACT_MY_DEVELOPER = "<i>𝕾𝖔𝖒𝖊𝖙𝖍𝖎𝖓𝖌 𝖂𝖗𝖔𝖓𝖌 𝕮𝖔𝖓𝖙𝖆𝖈𝖙 𝕸𝖞 𝕯𝖊𝖛𝖊𝖑𝖔𝖕𝖊𝖗 🤯</i>"
-    START_TEXT = "<i>𝕿𝖍𝖎𝖘 𝖎𝖘 𝖆 𝕾𝖎𝖒𝖕𝖑𝖊 𝖋𝖎𝖑𝖊 𝖗𝖊𝖓𝖆𝖒𝖊𝖗 & 𝕱𝖎𝖑𝖊 𝕮𝖔𝖓𝖛𝖊𝖗𝖙𝖊𝖗 𝖇𝖔𝖙 𝖜𝖎𝖙𝖍 𝖕𝖊𝖗𝖒𝖆𝖓𝖊𝖓𝖙 𝖙𝖍𝖚𝖒𝖇𝖓𝖆𝖎𝖑 𝖘𝖚𝖕𝖕𝖔𝖗𝖙 💯</i>"
-    UPGRADE_TEXT = "<b>To upgrade your subscription <a href='https://t.me/TeleRoid14'>[ 𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞 ]</a></b>"
+    CONTACT_MY_DEVELOPER = "<i>❗❗𝓢𝓸𝓶𝓽𝓱𝓲𝓷𝓰 𝓦𝓻𝓸𝓷𝓰.𝓒𝓸𝓷𝓽𝓪𝓬𝓽 𝓜𝔂 𝓓𝓮𝓿𝓮𝓵𝓸𝓹𝓮𝓻❗❗🤯@SL_Jana_Team</i>"
+    START_TEXT = "<i>🤖𝓣𝓱𝓲𝓼 𝓲𝓼 𝓪 𝓢𝓲𝓶𝓹𝓵𝓮 𝓕𝓲𝓵𝓮 𝓡𝓮𝓷𝓪𝓶𝓮𝓻 & 𝓕𝓲𝓵𝓮 𝓒𝓸𝓷𝓿𝓮𝓻𝓽𝓮𝓻 𝓑𝓸𝓽 𝔀𝓲𝓽𝓱 𝓹𝓮𝓻𝓶𝓪𝓷𝓮𝓷𝓽 𝓽𝓱𝓾𝓶𝓫𝓷𝓪𝓲𝓵 𝓼𝓾𝓹𝓹𝓸𝓻𝓽🤖💯@𝕊𝕃_𝕁𝕒𝕟𝕒_ℝ𝕖𝕟𝕒𝕞𝕖_𝔹𝕠𝕥</i>"
+    UPGRADE_TEXT = "<b>a⬆️𝐓𝐨 𝐮𝐩𝐠𝐫𝐚𝐝𝐞 𝐲𝐨𝐮𝐫 𝐬𝐮𝐛𝐬𝐜𝐫𝐢𝐩𝐭𝐢𝐨𝐧⬆️<a href='https://t.me/SL_Jana_Team'>SL_Jana_Team</a></b>"
